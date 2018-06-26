49427d4a2b100125f01e723b1980de06c2be8ef0
LeGa14 <glemonjr68@gmail.com>
2018-06-26 17:38:07 -0400

    user api new route working

--- routes/users.js
+++ routes/users.js
@@ -12,12 +12,21 @@ router.get('/', (req, res) => {
   })
 })
 
+// SHOW a single USER
 router.get('/:id', (req, res) => {
   const user = UserModel.findById(req.params.id).then((user) => {
 
     res.send({
       user
     })
+  })
+})
+
+//NEW USER
+router.post('/', (req, res) => {
+  const newUser = new UserModel(req.body)
+  newUser.save().then((user) => {
+    res.send({user})
   })
 })
 
