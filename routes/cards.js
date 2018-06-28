e8cc86b63d58178cda7533196005946c9bd1361a
LeGa14 <glemonjr68@gmail.com>
2018-06-27 20:40:30 -0400

    cards-model index-route added and working

--- routes/cards.js
+++ routes/cards.js
@@ -5,6 +5,26 @@ const { CardModel } = require('../db/schema')
 const router = express.Router({ mergeParams: true })
 
 
-userRouter.use('/users/:userId/cards', router)
+// userRouter.use('/users/:userId/cards', router)
+
+// INDEX CARDs
+router.get('/', (req, res) => {
+  // UserModel.find().then((users) => {
+  console.log('The User ID is...')
+  console.log(req.params.userId)
+  UserModel.findById(req.params.userId)
+    .then((user) => {
+      user = user
+      console.log(`This user's name is`)
+      console.log(user.username)
+      console.log(`They have...`)
+      console.log(user.cards.length, `Cards`)
+      usersCards = user.cards
+    }).then((usersChars) => {
+      res.send({
+        usersCards
+      })
+    })
+})
 
 module.exports = router
