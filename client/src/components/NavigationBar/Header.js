762d7909e3034eb525dc7ae56ca194e68b24f135
LeGa14 <glemonjr68@gmail.com>
2018-06-28 12:14:15 -0400

    added Header component to NavBar

--- client/src/components/NavigationBar/Header.js
+++ client/src/components/NavigationBar/Header.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 
-class SplashScreen extends Component {
+class Header extends Component {
     render() {
         return(
             <div>
-                <h3>Hello from the Splash Screen</h3>
+                <h3>Hello from the NavBar's Header Component</h3>
             </div>
         )
     }
 }
 
-export default SplashScreen
+export default Header
--- client/src/components/NavigationBar/NavigationBar.js
+++ client/src/components/NavigationBar/NavigationBar.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from './Header'
 
 class NavigationBar extends Component {
     render() {
         return (
             <div>
                 <h2>Hello from the NavigationBar Component</h2>
+                <Header />
             </div>
         );
     }
