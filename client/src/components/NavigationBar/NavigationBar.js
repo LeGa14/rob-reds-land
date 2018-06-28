c140476644ef72727165a4bdfecf273fc51f20f0
LeGa14 <glemonjr68@gmail.com>
2018-06-28 13:36:49 -0400

    basic style added s/t nav-bar is formatted

--- client/src/components/NavigationBar/NavigationBar.js
+++ client/src/components/NavigationBar/NavigationBar.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
+
 import Header from './Header'
 import UserStatus from './UserStatus'
 import Links from './Links'
 
+const NavStructure = styled.div`
+div.NavParts {
+    display: flex;
+    flex-directions: column;
+    justify-content: space-between;
+    max-width: 30vw;
+}
+    
+div.NavComponents {
+    display: flex;
+    flex-direction: row;
+    justify-content: space between;
+    max-width:30vw;
+}
+`
+
 class NavigationBar extends Component {
     render() {
         return (
-            <div>
+            <div className="NavigationBar">
+            <NavStructure className="NavParts">
                 <h2>Hello from the NavigationBar Component</h2>
-                <Header />
-                <Links />
-                <UserStatus />
+                <div className="NavComponents">
+                    <Header />
+                    <Links />
+                    <UserStatus />
+                </div>
+            </NavStructure>
             </div>
         );
     }
