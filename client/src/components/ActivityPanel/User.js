199209a109e6622eb6849177d11765b8fd690cee
LeGa14 <glemonjr68@gmail.com>
2018-06-28 14:10:10 -0400

    characters-component added to user

--- client/src/components/ActivityPanel/User.js
+++ client/src/components/ActivityPanel/User.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 
+import Characters from './User/Characters'
+
 class User extends Component {
     render() {
         return(
             <div>
                 <h3>Hello from the User Component</h3>
+                <Characters />
             </div>
         )
     }
---
+++ client/src/components/ActivityPanel/User/Characters.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Characters extends Component {
+    render() {
+        return(
+            <div>
+                <h4>Hello from the Characters Component</h4>
+            </div>
+        )
+    }
+}
+
+export default Characters
