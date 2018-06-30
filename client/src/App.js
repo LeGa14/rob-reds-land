6bac4472c299d110972138c9bfc961bee93d620d
LeGa14 <glemonjr68@gmail.com>
2018-06-30 17:34:55 -0400

    api successfully sent to front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import styled from 'styled-components'
+import axios from 'axios'
 
 import NavigationBar from './components/NavigationBar'
 import ActivityPanel from './components/ActivityPanel'
@@ -8,11 +9,25 @@ import ActivityPanel from './components/ActivityPanel'
 const ComponentMarker = styled.div`
 .component {
   margin: 10px;
-  border: 10px solid blue;
+  border: 1px solid purple;
 }
 `
 
 class App extends Component {
+  state = {
+    users: [],
+    characters: [],
+    cards: [],
+    models: [ 'users', 'characters', 'cards']
+  }
+
+  componentDidMount() {
+    axios.get('/api/users').then((res) => {
+      this.setState({users: res.data.users})
+      console.log(res.data)
+    })
+  }
+
   render() {
     return (
       <ComponentMarker className="App">
